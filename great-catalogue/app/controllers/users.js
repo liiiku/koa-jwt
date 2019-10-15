7e8419664870a328899fb31d88efea5e2c39d629
ruinan.li02 <ruinan.li02@ele.me>
2019-10-15 17:21:00 +0800

    feat: 简单的koa错误处理

--- great-catalogue/app/controllers/users.js
+++ great-catalogue/app/controllers/users.js
@@ -2,9 +2,13 @@ const db = [{ name: '李雷' }];
 
 class UsersCtl {
   find(ctx) {
+    a.b
     ctx.body = db;
   }
   findById(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412, '先决条件失败：id大于等于数组长度了');    
+    }
     ctx.body = db[ctx.params.id * 1];
   }
   create(ctx) {
